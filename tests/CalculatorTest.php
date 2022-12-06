fd88a66f20dda66580ef845b95ebb6409cf37c75
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-12-06 12:39:42 +0600

    Day 5 - Setup and teardown

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -5,47 +5,66 @@ use PHPUnit\Framework\TestCase;
 
 class CalculatorTest extends TestCase
 {
+    public $calculator;
+
+    /**
+     * This method is called after the last test of this test class is run.
+     */
+    public static function tearDownAfterClass(): void
+    {
+    }
+
+    /**
+     * This method is called before each test.
+     */
+    protected function setUp(): void
+    {
+        $this->calculator = new Calculator();
+    }
+
+    /**
+     * This method is called after each test.
+     */
+    protected function tearDown(): void
+    {
+        $this->calculator->numbers = [];
+
+        // TRUCATE THE TABLE.
+    }
+
     public function testIsCalculatorInstance()
     {
-        $calculator = new Calculator();
-        $calculatorInstance = $calculator->getInstance();
+        $calculatorInstance = $this->calculator->getInstance();
 
         $this->assertInstanceOf(Calculator::class, $calculatorInstance);
     }
 
     public function testaddTwoNumber()
     {
-        $calculator = new Calculator();
-        // $sum = $calculator->addNumber(100, 200);
-
-        $this->assertSame(300, $calculator->addNumber(100, 200));
-        $this->assertEquals('300', $calculator->addNumber(100, 200));
+        $this->assertSame(300, $this->calculator->addNumber(100, 200));
+        $this->assertEquals('300', $this->calculator->addNumber(100, 200));
     }
 
     public function testCanAddTwoNumber()
     {
-        $calculator = new Calculator();
+        $this->assertTrue($this->calculator->canAddTwoNumber(4, 5));
 
-        $this->assertTrue($calculator->canAddTwoNumber(4, 5));
-
-        $this->assertNotTrue($calculator->canAddTwoNumber('akash', 5));
+        $this->assertNotTrue($this->calculator->canAddTwoNumber('akash', 5));
     }
 
     public function testCountNumbers()
     {
-        $calculator = new Calculator();
-        $this->assertCount(0, $calculator->numbers);
+        $this->assertCount(0, $this->calculator->numbers);
 
-        $this->assertSame(300, $calculator->addNumber(100, 200));
-        $this->assertCount(2, $calculator->numbers);
+        $this->assertSame(300, $this->calculator->addNumber(100, 200));
+        $this->assertCount(2, $this->calculator->numbers);
     }
 
     public function testCountNumbersAsArray()
     {
-        $calculator = new Calculator();
-        $this->assertSame(300, $calculator->addNumber(100, 200));
-        $this->assertIsArray($calculator->numbers);
-        $this->assertArrayHasKey(100, $calculator->numbers);
-        $this->assertArrayHasKey(200, $calculator->numbers);
+        $this->assertSame(300, $this->calculator->addNumber(100, 200));
+        $this->assertIsArray($this->calculator->numbers);
+        $this->assertArrayHasKey(100, $this->calculator->numbers);
+        $this->assertArrayHasKey(200, $this->calculator->numbers);
     }
 }
